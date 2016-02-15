0e9e75864ec4481b4ce2a5a5369e9b33f98de562
Ubuntu <ubuntu@ip-172-31-27-128.ec2.internal>
2016-02-15 17:49:42 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-fs.readFile('index.html', 'utf8', function (err,data) {
+fs.readFile('./bitstarter/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
