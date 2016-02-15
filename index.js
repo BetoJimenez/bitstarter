37d99267ebb751e66f53dad355a2f2e92fd94e29
Ubuntu <ubuntu@ip-172-31-27-128.ec2.internal>
2016-02-15 17:40:53 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var express = require('express')
-var app = express()
+var fs = require('fs');
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World3!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
