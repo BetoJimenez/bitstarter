6071cfdb0649591aa6a604d6a66723a542b3ddee
Mario Alberto Jimenez Duarte <majduarte@yahoo.com.mx>
2016-02-18 10:30:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var fs = require('fs');
-fs.readFile('./bitstarter/index.html', 'utf8', function (err,data) {
+var express = require('express')
+var app = express()
+//var fs = require('fs')
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World3!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+/* fs.readFile('./bitstarter/index.html', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
   console.log(data);
-});
-
+}); */
